2b28921a97d2ecccafa17462f3d0c8f257b65315
Christopher Kim <137121884+christopherminkim@users.noreply.github.com>
2023-11-06 16:34:08 -0800

    Add close function to each task

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const taskInput = document.querySelector('.task-input');
 const taskList = document.querySelector('.task-list');
 addTask.addEventListener('click', () => {
   const task = document.createElement('li');
+  const closeBtn = document.createElement('button');
+  closeBtn.append('x');
+  closeBtn.addEventListener('click', () => {
+    task.remove();
+  });
   task.append(taskInput.value);
+  task.append(closeBtn);
   taskList.append(task);
 });
-// add function to get rid of tasks, mark/check off tasks, and css
+// mark/check off tasks, and css
